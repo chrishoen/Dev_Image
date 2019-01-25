9ba1298008dc7467b83e8541bc67cb9a774270d0
chrishoen <chris123hoen@gmail.com>
2019-01-25 11:57:40 -0500

    added GenImage

---
+++ GenImage/MainInit.cpp
@@ -0,0 +1,68 @@
+#include "stdafx.h"
+
+
+#include "someImageParms.h"
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Initialize
+
+void main_initialize(int argc,char** argv)
+{
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Initialize print.
+
+   // Initialize print facility
+   Prn::resetPrint();
+   Prn::initializePrint();
+   
+   // Initialize print filters
+   Prn::setFilter(Prn::ThreadRun1,  true);
+   Prn::setFilter(Prn::ThreadRun2,  false);
+   Prn::setFilter(Prn::ThreadRun3,  true);
+   Prn::setFilter(Prn::ThreadRun4,  true);
+
+   Prn::setFilter(Prn::ProcRun1,    true);
+   Prn::setFilter(Prn::ProcRun2,    true);
+   Prn::setFilter(Prn::ProcRun3,    false);
+   Prn::setFilter(Prn::ProcRun4,    true);
+
+   Prn::setFilter(Prn::ViewRun1,    true, 1);
+   Prn::setFilter(Prn::ViewRun2,    true, 1);
+   Prn::setFilter(Prn::ViewRun3,    false,1);
+   Prn::setFilter(Prn::ViewRun4,    true, 1);
+
+   Prn::setFilter(Prn::QCallInit1, true);
+   Prn::setFilter(Prn::QCallInit2, true);
+   Prn::setFilter(Prn::QCallRun1,  false);
+   Prn::setFilter(Prn::QCallRun2,  false);
+   Prn::setFilter(Prn::QCallRun3,  false);
+   Prn::setFilter(Prn::QCallRun4,  false);
+
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Open settings file.
+
+   Some::gImageParms.reset();
+   Some::gImageParms.readSection("default");
+
+   Prn::print(0,"GenImage*******************************************BEGIN");
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Finalize
+
+void main_finalize()
+{
+   Prn::print(0,"GenImage*******************************************END");
+
+   // Close print
+   Prn::finalizePrint();
+}
+
