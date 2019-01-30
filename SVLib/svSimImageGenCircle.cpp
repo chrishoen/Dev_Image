5f57a1ea8efa0d57c5e01e32603d61f61dc08157
chrishoen <chris123hoen@gmail.com>
2019-01-30 15:47:19 -0500

    draw circle works

--- SVLib/svSimImageGenCircle.cpp
+++ SVLib/svSimImageGenCircle.cpp
@@ -51,6 +51,26 @@ void SimImageGenCircle::doGenerateImage(
    Prn::print(0, "SimImageGenCircle::doGenerateImage");
    // Create an image filled with all zeros.
    BaseClass::doCreateZeroImage(aImage);
+
+   // Circle variables. 
+   int tCenterRow = mP->mImageSize.mRows / 2;
+   int tCenterCol = mP->mImageSize.mCols / 2;
+   cv::Point tCenter(tCenterCol,tCenterRow);
+   int tRadius = mP->mImageB;
+   cv::Scalar tColor(255.0);
+   int tThickness = -1;
+   int tLineType = 8;
+   int tShift = 0;
+
+   // Draw circle.
+   cv::circle(
+      aImage,
+      tCenter,
+      tRadius,
+      tColor,
+      tThickness,
+      tLineType,
+      tShift);
 }
 
 //******************************************************************************
