82ded3a895fd751505952c6518c1055f01ae2379
chrishoen <chris123hoen@gmail.com>
2019-02-01 00:00:07 -0500

    added project Numbers

---
+++ Numbers/CmdLineExec.h
@@ -0,0 +1,41 @@
+#pragma once
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+#include "risCmdLineExec.h"
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+class CmdLineExec : public Ris::BaseCmdLineExec
+{
+public:
+
+  CmdLineExec();
+
+  void reset();
+  void execute(Ris::CmdLineCmd* aCmd);
+
+  void executeGo1(Ris::CmdLineCmd* aCmd);
+  void executeGo2(Ris::CmdLineCmd* aCmd);
+  void executeGo3(Ris::CmdLineCmd* aCmd);
+  void executeGo4(Ris::CmdLineCmd* aCmd);
+  void executeGo5(Ris::CmdLineCmd* aCmd);
+  void executeGo6(Ris::CmdLineCmd* aCmd);
+  void executeGo7(Ris::CmdLineCmd* aCmd);
+  void executeGo8(Ris::CmdLineCmd* aCmd);
+  void executeGo9(Ris::CmdLineCmd* aCmd);
+  void executeGo10(Ris::CmdLineCmd* aCmd);
+  void executeGo21(Ris::CmdLineCmd* aCmd);
+  void executeGo22(Ris::CmdLineCmd* aCmd);
+
+  void executeParms(Ris::CmdLineCmd* aCmd);
+};
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
