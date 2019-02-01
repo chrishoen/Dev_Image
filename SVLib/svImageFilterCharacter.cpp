994d78c881136fd3455e05bd266a23d8c42f6239
chrishoen <chris123hoen@gmail.com>
2019-01-31 21:03:02 -0500

    more character filter

--- SVLib/svImageFilterCharacter.cpp
+++ SVLib/svImageFilterCharacter.cpp
@@ -10,6 +10,8 @@ Description:
 
 #include "svSysParms.h"
 #include "svDefs.h"
+#include "svPixelFunctions.h"
+#include "svImageWrapper.h"
 
 #include "svImageFilterCharacter.h"
 
@@ -48,6 +50,29 @@ void ImageFilterCharacter::doFilterImage(
 {
    Prn::print(0, "ImageFilterCharacter::doFilterImage");
 
+   // Create an output image filled with all zeros.
+   aOutputImage = cv::Mat::zeros(aInputImage.size(), CV_8UC1);
+
+   // Image wrappers.
+   ImageWrapper tInputImage(aInputImage);
+   ImageWrapper tOutputImage(aOutputImage);
+
+
+   // Loop through all of the rows and columns of the input image.
+   for (SV::RCIndexLoop tIndex(tInputImage.rcSize()); tIndex.test(); tIndex.next())
+   {
+      // Get the pixel value of the input image at the index.
+      uchar tValue = tInputImage.at(tIndex());
+
+      if (tValue == 255)
+      {
+         tOutputImage.at(tIndex()) = 7;
+      }
+      else
+      {
+         tOutputImage.at(tIndex()) = 2;
+      }
+   }
 }
 
 //******************************************************************************
