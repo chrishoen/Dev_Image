e02a10715349a800c9a172a0a00b9151c98ea39c
chrishoen <chris123hoen@gmail.com>
2019-02-12 12:53:06 -0500

    added project NNRuleFilter

---
+++ NNRuleFilter/MainInit.cpp
@@ -0,0 +1,113 @@
+
+#include "stdafx.h"
+
+#include "tsThreadServices.h"
+#include "risThreadsProcess.h"
+
+#include "svSysParms.h"
+#include "svImageParms.h"
+#include "svSimParms.h"
+#include "displayParms.h"
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Initialize program resources.
+
+void main_initialize(int argc,char** argv)
+{
+   printf("NNRuleFilter Program********************************************BEGIN\n");
+   printf("NNRuleFilter Program********************************************BEGIN\n");
+   printf("NNRuleFilter Program********************************************BEGIN\n\n");
+
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Initialize thread services.
+
+   TS::reset();
+   TS::setProgramName("NNRuleFilter");
+   TS::setProgramPrintLevel(TS::PrintLevel(0, 3));
+   TS::initialize();
+
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Enter process.
+
+   // Set program process for high priority.
+   Ris::Threads::enterProcessHigh();
+
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Initialize print facility.
+
+   // Initialize print.
+   Prn::resetPrint();
+   Prn::useConsole(1);
+   Prn::initializePrint();
+
+   // Initialize print filters.
+   Prn::setFilter(Prn::ThreadInit1, true);
+   Prn::setFilter(Prn::ThreadInit2, false);
+   Prn::setFilter(Prn::ThreadRun1, true);
+   Prn::setFilter(Prn::ThreadRun2, true);
+   Prn::setFilter(Prn::ThreadRun3, true);
+   Prn::setFilter(Prn::ThreadRun4, true);
+
+   Prn::setFilter(Prn::View01,          true);
+   Prn::setFilter(Prn::View02,          true);
+   Prn::setFilter(Prn::View11,          true,  1);
+   Prn::setFilter(Prn::View12,          true,  1);
+   Prn::setFilter(Prn::View21,          true,  2);
+   Prn::setFilter(Prn::View21,          true,  2);
+
+   Prn::setFilter(Prn::Show1, true);
+
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Read parameters files.
+
+   // Read parameters files.
+   SV::gSysParms.reset();
+   SV::gSysParms.readSection("Default");
+
+   // Read parameters files.
+   SV::gImageParms.reset();
+   SV::gImageParms.readSection("Default");
+
+   // Read parameters files.
+   SV::gSimParms.reset();
+   SV::gSimParms.readSection("Default");
+
+   // Read parameters files.
+   Display::gParms.reset();
+   Display::gParms.readSection("Default");
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Finalize.
+
+void main_finalize()
+{
+   // Finalize print facility.
+   Prn::finalizePrint();
+
+   // Exit process.
+   Ris::Threads::exitProcess();
+
+   // Finalize thread services.
+   TS::finalize();
+
+   // Done.
+   printf("\n");
+   printf("NNRuleFilter Program********************************************END\n\n");
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
---
+++ NNRuleFilter/Simulate.cpp
@@ -0,0 +1,158 @@
+/*==============================================================================
+Description:
+==============================================================================*/
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+#include "stdafx.h"
+
+#include "svImageParms.h"
+#include "svSimParms.h"
+
+#include "svSimImageGenerator.h"
+
+#include "svImageFunctions.h"
+#include "svImageShow.h"
+
+#include "displayFunctions.h"
+
+#include "Simulate.h"
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+Simulate::Simulate()
+{
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// This selects a test.
+
+void Simulate::doRun(int aCode)
+{
+   switch (aCode)
+   {
+   case 1: doRun1(); break;
+   case 2: doRun2(); break;
+   case 3: doRun3(); break;
+   }
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// This runs a test.
+
+void Simulate::doRun1()
+{
+   Prn::print(Prn::View11, "RUN1********************************************************************");
+   Prn::print(Prn::View11, "RUN1********************************************************************");
+   Prn::print(Prn::View11, "RUN1********************************************************************");
+
+   // Generate simulated image.
+   SV::SimImageGenerator tGenerator(&SV::gSimParms.mImageGenParms);
+   tGenerator.doGenerateImage(mInputImage);
+   SV::showImageInfo(Prn::View11,"InputImage", mInputImage);
+
+   // Display the input image.
+   Display::showImage(mInputImage);
+
+   // Save simulated image.
+   SV::writeImageToFile(mInputImage, SV::gImageParms.mInputImageFileName);
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// This runs a test.
+
+void Simulate::doRun2()
+{
+   Prn::print(Prn::View01, "RUN2********************************************************************");
+   Prn::print(Prn::View01, "RUN2********************************************************************");
+   Prn::print(Prn::View01, "RUN2********************************************************************");
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// This runs a test.
+
+void Simulate::doRun3()
+{
+   Prn::print(Prn::View01, "RUN3********************************************************************");
+   Prn::print(Prn::View01, "RUN3********************************************************************");
+   Prn::print(Prn::View01, "RUN3********************************************************************");
+
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// This runs a test.
+
+void Simulate::doTest(int aCode)
+{
+   switch (aCode)
+   {
+   case 1: doTest1(); break;
+   case 2: doTest2(); break;
+   }
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// This runs a test.
+
+void Simulate::doTest1()
+{
+   // Read a test image file to the input image.
+   SV::readImageFromFile(mInputImage, SV::gImageParms.mTestImageFileName);
+   SV::showImageInfo(Prn::View11, "InputImage", mInputImage);
+
+   // Display the input image.
+   Display::showImage(mInputImage);
+}
+
+void Simulate::doTest2()
+{
+   // Read a test image file to the input image.
+   SV::readImageFromFile(mInputImage, SV::gImageParms.mTestImageFileName);
+   SV::showImageInfo(Prn::View11, "InputImage", mInputImage);
+
+   // Display the input image.
+   Display::showImage(SV::gImageParms.mTestImageFileName);
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// This runs a test.
+
+void Simulate::doShow(int aCode)
+{
+   switch (aCode)
+   {
+   case 11: SV::showImageTable1("InputImage", mInputImage); break;
+   case 12: SV::showImageTable2("InputImage", mInputImage); break;
+   case 13: SV::showImageTable3("InputImage", mInputImage); break;
+
+   case 21: SV::showImageTable1("OutputImage", mOutputImage); break;
+   case 22: SV::showImageTable2("OutputImage", mOutputImage); break;
+   case 23: SV::showImageTable3("OutputImage", mOutputImage); break;
+   }
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Convert simulated image to target image and display.
+//Display::showImage(mOutputImage);
+// Save simulated image.
+//SV::writeImageToFile(mOutputImage, SV::gImageParms.mOutputImageFileName);
