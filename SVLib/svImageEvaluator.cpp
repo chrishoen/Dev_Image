4668c242f4170df0fdae058e02f396d3a1138278
chrishoen <chris123hoen@gmail.com>
2019-02-19 20:04:00 -0500

    added svImageEvaluator and svImageResults

---
+++ SVLib/svImageEvaluator.cpp
@@ -0,0 +1,107 @@
+/*==============================================================================
+Description:
+==============================================================================*/
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+#include "stdafx.h"
+
+#include "svImageWrapper.h"
+#include "svRCLoop.h"
+
+#include "svImageEvaluator.h"
+
+namespace SV
+{
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+ImageEvaluator::ImageEvaluator()
+{
+   reset();
+}
+
+void ImageEvaluator::reset()
+{
+   mResults = 0;
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Evaluate the input image. Calculate the row and column metrics results.
+
+void ImageEvaluator::doEvaluateImage(
+   cv::Mat&       aInputImage,    // Input
+   ImageResults&  aResults)       // Output
+{
+   //******************************************************************************
+   //******************************************************************************
+   //******************************************************************************
+   // Initialize.
+
+   // Variables.
+   aResults.reset();
+   
+   // Inputs and outputs.
+   mInput.set(aInputImage);
+   mResults = &aResults;
+
+   //******************************************************************************
+   //******************************************************************************
+   //******************************************************************************
+   // Loop through the image.
+
+   // Loop through the image. Ignore the top and bottom rows and ignore
+   // the left and right edge columns.
+   SV::RCIndexLoop tLoop(RCIndex(1, 1), aInputImage.rows - 2, aInputImage.cols - 2);
+   while (tLoop.loop())
+   {
+      // Advance.
+      mResults->mTotalCount++;
+
+      // Evaluate each pixel that is high.
+      if (mInput.at(tLoop()) != 0)
+      {
+         doEvaluateHighPixel(tLoop());
+      }
+   }
+}
+
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Evaluate a high pixel. Calculate the row and column metrics results.
+
+void ImageEvaluator::doEvaluateHighPixel(RCIndex aX)
+{
+   // Advance.
+   mResults->mHighCount++;
+
+   // Store results for first evaluation.
+   if (mResults->mFirst)
+   {
+      mResults->mFirst = false;
+      mResults->mRowMin = aX;
+      mResults->mRowMax = aX;
+      mResults->mColMin = aX;
+      mResults->mColMax = aX;
+      return;
+   }
+
+   // Store results for other evaluations.
+   if (mResults->mRowMin.mRow <  aX.mRow) mResults->mRowMin = aX;
+   if (mResults->mRowMax.mRow >= aX.mRow) mResults->mRowMax = aX;
+   if (mResults->mColMin.mCol <  aX.mCol) mResults->mColMin = aX;
+   if (mResults->mColMax.mCol >= aX.mCol) mResults->mColMax = aX;
+}
+
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+}//namespace
