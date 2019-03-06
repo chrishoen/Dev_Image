a507d91b209e83098074bb990dccd9f45c8ece5a
chrishoen <chris123hoen@gmail.com>
2019-03-05 19:22:52 -0500

    added project Numbers

---
+++ Numbers/CmdLineExec.cpp
@@ -0,0 +1,165 @@
+
+#include "stdafx.h"
+
+#include "svSysParms.h"
+#include "svTestParms.h"
+
+#include "CmdLineExec.h"
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+CmdLineExec::CmdLineExec()
+{
+}
+
+void CmdLineExec::reset()
+{
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Base class override. Execute a command line command. It calls one of
+// the following specific command execution functions. This is called by
+// the owner of this object to pass command line commands to it. 
+
+void CmdLineExec::execute(Ris::CmdLineCmd* aCmd)
+{
+   if (aCmd->isCmd("GO1"))       executeGo1(aCmd);
+   if (aCmd->isCmd("GO2"))       executeGo2(aCmd);
+   if (aCmd->isCmd("GO3"))       executeGo3(aCmd);
+   if (aCmd->isCmd("GO4"))       executeGo4(aCmd);
+   if (aCmd->isCmd("GO5"))       executeGo5(aCmd);
+   if (aCmd->isCmd("Dia"))       executeDiamond(aCmd);
+   if (aCmd->isCmd("Parms"))     executeParms(aCmd);
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeGo1(Ris::CmdLineCmd* aCmd)
+{
+   aCmd->setArgDefault(1, 2);
+   aCmd->setArgDefault(2, 3);
+   aCmd->setArgDefault(3, 5);
+
+   int tLoopRow = aCmd->argInt(1);
+   int tNumRow = aCmd->argInt(2);
+   int tNumCol = aCmd->argInt(3);
+
+   int tRowCount = tLoopRow * tNumRow - tNumRow / 2;
+   int tColCount = tLoopRow * tNumCol - tNumCol / 2;
+
+   for (int tRow = 0; tRow < tRowCount; tRow++)
+   {
+      int tRowIndex = (tRow + tNumRow / 2) / tNumRow;
+      printf("Row %3d\n", tRowIndex);
+      printf("           ");
+      for (int tCol = 0; tCol < tColCount; tCol++)
+      {
+         int tColIndex = (tCol + tNumCol / 2) / tNumCol;
+         bool tFlag = tColIndex <= tLoopRow - tRowIndex - 1;
+         if (tFlag)
+         {
+            printf("Col  %3d  ", tColIndex);
+         }
+         else
+         {
+            printf("...  %3d  ", tColIndex);
+         }
+      }
+      printf("\n");
+   }
+   printf("\n");
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeGo2(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeGo3(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeGo4(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeGo5(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeDiamond(Ris::CmdLineCmd* aCmd)
+{
+   aCmd->setArgDefault(1, 2);
+   aCmd->setArgDefault(2, 3);
+   aCmd->setArgDefault(3, 5);
+
+   int tLoopRow = aCmd->argInt(1);
+   int tNumRow = aCmd->argInt(2);
+   int tNumCol = aCmd->argInt(3);
+
+   int tRowCount = tLoopRow * tNumRow - tNumRow / 2;
+   int tColCount = tLoopRow * tNumCol - tNumCol / 2;
+
+   for (int tRow = 0; tRow < tRowCount; tRow++)
+   {
+      int tRowIndex = (tRow + tNumRow / 2) / tNumRow;
+      printf("Row %3d\n", tRowIndex);
+      printf("           ");
+      for (int tCol = 0; tCol < tColCount; tCol++)
+      {
+         int tColIndex = (tCol + tNumCol / 2) / tNumCol;
+         bool tFlag = tColIndex <= tLoopRow - tRowIndex - 1;
+         if (tFlag)
+         {
+            printf("Col  %3d  ", tColIndex);
+         }
+         else
+         {
+            printf("...  %3d  ", tColIndex);
+         }
+      }
+      printf("\n");
+   }
+   printf("\n");
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeParms(Ris::CmdLineCmd* aCmd)
+{
+   SV::gSysParms.reset();
+   SV::gSysParms.readSection("default");
+   SV::gSysParms.show();
+
+   SV::gTestParms.reset();
+   SV::gTestParms.readSection("default");
+   SV::gTestParms.show();
+}
+
